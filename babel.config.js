9e7f9ff1a7b1ac5986c2f6761f271879708023a5
lijun1999 <2562402511@qq.com>
2020-09-08 22:41:55 +0800

    打包时去除项目内console

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,21 @@ module.exports = {
   presets: [
     "@vue/cli-plugin-babel/preset"
   ],
-  plugins: [
-    [
-      "component",
-      {
-        libraryName: "element-ui",
-        styleLibraryName: "theme-chalk"
-      }
-    ]
-  ]
+  'env': {
+    'development': {
+      'plugins': ['dynamic-import-node']
+    },
+    'production': {
+      'plugins': [
+        'transform-remove-console',
+        [
+              "component",
+              {
+                libraryName: "element-ui",
+                styleLibraryName: "theme-chalk"
+              }
+            ]
+      ]
+    }
+  }
 }
